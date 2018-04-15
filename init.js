36cdd939cbe8bc8ae9eee2f0a62a379ec52b287b
Carlos Alba <soradev4@gmail.com>
2018-04-14 18:28:43 -0700

    stuff

---
+++ init.js
@@ -0,0 +1,10 @@
+ // Initialize Firebase
+ var config = {
+     apiKey: "AIzaSyBnK-EWNzq4km5AbISAPJ5gB2btrsnSt7E",
+     authDomain: "cupertino-social-media.firebaseapp.com",
+     databaseURL: "https://cupertino-social-media.firebaseio.com",
+     projectId: "cupertino-social-media",
+     storageBucket: "cupertino-social-media.appspot.com",
+     messagingSenderId: "766385527977"
+ };
+ firebase.initializeApp(config);
