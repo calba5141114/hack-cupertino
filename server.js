90493257d98262e24020c841723b2086a401937e
Carlos Alba <soradev4@gmail.com>
2018-04-14 20:19:04 -0700

    woh daddy

---
+++ server.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+app.use(express.static('public'));
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'));
