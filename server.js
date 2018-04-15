5647fd9b60afb2cb4d99b64db12a9357e39596ce
Carlos Alba <soradev4@gmail.com>
2018-04-14 20:33:02 -0700

    bengal

--- server.js
+++ server.js
@@ -1,6 +1,16 @@
 const express = require('express');
 const app = express();
 app.use(express.static('public'));
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public', 'index.html')));
+
+app.get('/', function (req, res) {
+    res.sendFile(__dirname + 'index.html');
+});
+app.get('/feed', function (req, res) {
+    res.sendFile(__dirname + '/public/pages/feed.html');
+});
+
+app.get('/profile', function (req, res) {
+    res.sendFile(__dirname + '/public/pages/profile.html');
+});
 
 app.listen(3000, () => console.log('Example app listening on port 3000!'));
